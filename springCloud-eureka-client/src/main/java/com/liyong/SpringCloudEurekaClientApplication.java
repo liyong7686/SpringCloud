4258a28648bb445b55b5ba58f18e3bfba036e60a
liyong7686 <768665210@qq.com>
2019-01-11 15:37:33 +0800

    更新

--- springCloud-eureka-client/src/main/java/com/liyong/SpringCloudEurekaClientApplication.java
+++ springCloud-eureka-client/src/main/java/com/liyong/SpringCloudEurekaClientApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class SpringCloudEurekaClientApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {   
 		SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
 	}
 
